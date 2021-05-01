b836435e2e2723c3391677703e9f7705279456a2
gabrielbaron16 <gabrielbu161@gmail.com>
2021-04-30 23:16:35 -0400

    Se Muestran las Transacciones para un Inventario en Específico

--- src/routes/DeviceTransaction.ts
+++ src/routes/DeviceTransaction.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express'
 import { sessionCheck } from '@middlewares/auth/auth';
-import { findAllDeviceTransactionCtrl, findBcDeviceTransactionCtrl } from '@controllers/DeviceTransactions';
+import {
+    findAllDeviceTransactionCtrl,
+    findBcDeviceTransactionCtrl,
+    findInventoryDeviceTransactionCtrl
+} from '@controllers/DeviceTransactions';
 
 const router = Router();
 
 router.get('/', [sessionCheck], findAllDeviceTransactionCtrl);
 router.get('/:id', [sessionCheck], findBcDeviceTransactionCtrl);
+router.get('/inventory/:id', [sessionCheck], findInventoryDeviceTransactionCtrl);
 
 export default router;
